122c84884becf9adae46f8c60d973377c034d90d
PhamHungXuanSang <phamhungxuansang13082002@gmail.com>
2024-05-13 16:32:34 +0700

    Chiều 13/05 ver 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -79,35 +79,35 @@ export default function App() {
             newMessage.shouldShake = true;
             const sound = new Audio(notificationSound);
             sound.play();
-            // toast.custom(
-            //     (t) => (
-            //         <div
-            //             className={`${
-            //                 t.visible ? 'animate-enter' : 'animate-leave'
-            //             } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-            //         >
-            //             <div className="flex-1 w-0 p-4">
-            //                 <div className="ml-3 flex-1">
-            //                     <p className="text-sm font-medium text-gray-900">New Message</p>
-            //                     <p className="mt-1 text-sm text-gray-500">{newMessage.message}</p>
-            //                 </div>
-            //             </div>
-            //             <div className="flex border-l border-gray-200">
-            //                 <button
-            //                     onClick={() => {
-            //                         toast.dismiss(t.id);
-            //                     }}
-            //                     className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            //                 >
-            //                     Close
-            //                 </button>
-            //             </div>
-            //         </div>
-            //     ),
-            //     {
-            //         duration: 3000,
-            //     },
-            // );
+            toast.custom(
+                (t) => (
+                    <div
+                        className={`${
+                            t.visible ? 'animate-enter' : 'animate-leave'
+                        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+                    >
+                        <div className="flex-1 w-0 p-4">
+                            <div className="ml-3 flex-1">
+                                <p className="text-sm font-medium text-gray-900">New Chat Message</p>
+                                <p className="mt-1 text-sm text-gray-500">{newMessage.message}</p>
+                            </div>
+                        </div>
+                        <div className="flex border-l border-gray-200">
+                            <button
+                                onClick={() => {
+                                    toast.dismiss(t.id);
+                                }}
+                                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                ),
+                {
+                    duration: 3000,
+                },
+            );
             setMessages([...messages, newMessage]);
         };
 
@@ -115,6 +115,8 @@ export default function App() {
             dispatch(setCurrentUser({ ...currentUser, newNotification: true }));
             let { userAvatar, thumb, title, message, type } = data;
             if (filterStateMapping[type]) {
+                const sound = new Audio(notificationSound);
+                sound.play();
                 toast.custom(
                     (t) => (
                         <div
@@ -179,7 +181,7 @@ export default function App() {
                 socket.off('forcedLogout', handleForcedLogout);
             }
         };
-    }, [system, like, comment, reply, rate, subscriber, newBlog, messages, setMessages]);
+    }, [system, like, comment, reply, rate, subscriber, newBlog, messages, setMessages, currentUser]); // Thêm code currentUser vô coi sao
 
     return (
         <PayPalScriptProvider options={initialOptions}>
