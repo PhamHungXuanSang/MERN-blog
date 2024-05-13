67e3ab567d12ad32993220adea5d6429819a2e40
PhamHungXuanSang <phamhungxuansang13082002@gmail.com>
2024-05-13 15:58:23 +0700

    Chiều 13/05 ver 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -89,7 +89,7 @@ export default function App() {
                         <div className="flex-1 w-0 p-4">
                             <div className="ml-3 flex-1">
                                 <p className="text-sm font-medium text-gray-900">New Message</p>
-                                <p className="mt-1 text-sm text-gray-500">{newMessage}</p>
+                                <p className="mt-1 text-sm text-gray-500">{newMessage.message}</p>
                             </div>
                         </div>
                         <div className="flex border-l border-gray-200">
