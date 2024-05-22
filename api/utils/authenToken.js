81ad8b7284d2cbd58cf0c987c72c6ade66f95e6e
PhamHungXuanSang <phamhungxuansang13082002@gmail.com>
2024-05-23 00:30:55 +0700

    Xóa logic kiểm tra user block vì lỗi

--- api/utils/authenToken.js
+++ api/utils/authenToken.js
@@ -1,28 +1,28 @@
-import { io, userOnline } from '../index.js';
-import User from '../models/user.model.js';
+// import { io, userOnline } from '../index.js';
+// import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const authenToken = async (req, res, next) => {
     const userId = req.params.userId || req.body.userId || req.body._id;
     const token = req.cookies.access_token;
-    const user = await User.findById(userId).select('-password');
-    if (!user) {
-        return next(errorHandler(404, 'User not found'));
-    }
-    if (user.isBlocked) {
-        const socketId = userOnline.get(userId.toString());
-        if (socketId) {
-            io.to(socketId).emit('forcedLogout', 'Forced Logout');
-            userOnline.delete(userId.toString());
-        }
-    }
+    // const user = await User.findById(userId).select('-password');
+    // if (!user) {
+    //     return next(errorHandler(404, 'User not found'));
+    // }
+    // if (user.isBlocked) {
+    //     const socketId = userOnline.get(userId.toString());
+    //     if (socketId) {
+    //         io.to(socketId).emit('forcedLogout', 'Forced Logout');
+    //         userOnline.delete(userId.toString());
+    //     }
+    // }
     
-    if (!token) {
-        res.clearCookie('access_token');
-        userOnline.delete(userId?.toString());
-        return next(errorHandler(403, 'No access token provided.'));
-    }
+    // if (!token) {
+    //     res.clearCookie('access_token');
+    //     userOnline.delete(userId?.toString());
+    //     return next(errorHandler(403, 'No access token provided.'));
+    // }
 
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
